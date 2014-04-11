14fe661b3665b3b8e0707b1929030fc88f1471d1
Eric Appel <ema77@cornell.edu>
2014-04-11 17:05:11 -0400

    Adding classes

---
+++ Example/RGBColorSliderExample/RGBColorSliderExample/RGBEXViewController.m
@@ -0,0 +1,29 @@
+//
+//  RGBEXViewController.m
+//  RGBColorSliderExample
+//
+//  Created by Eric Appel on 4/11/14.
+//  Copyright (c) 2014 Eric Appel. All rights reserved.
+//
+
+#import "RGBEXViewController.h"
+
+@interface RGBEXViewController ()
+
+@end
+
+@implementation RGBEXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
