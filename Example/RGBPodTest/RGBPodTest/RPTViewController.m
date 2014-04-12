7d1698cf0519763abcf1398258c1e853d2598e7a
Eric Appel <ema77@cornell.edu>
2014-04-11 21:34:26 -0400

    Accidently added new xcodeproj

---
+++ Example/RGBPodTest/RGBPodTest/RPTViewController.m
@@ -0,0 +1,29 @@
+//
+//  RPTViewController.m
+//  RGBPodTest
+//
+//  Created by Eric Appel on 4/11/14.
+//  Copyright (c) 2014 Eric Appel. All rights reserved.
+//
+
+#import "RPTViewController.h"
+
+@interface RPTViewController ()
+
+@end
+
+@implementation RPTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
